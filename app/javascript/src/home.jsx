1ef0e7c02e8eeea3019ef1d8a488e707996f6a42
Matej Palenik <matejpale@gmail.com>
2021-08-15 17:41:21 +0200

    made a start on twitter homepage

--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import backgroundImage from "../images/twitter 2.png";
 
 import "./home.scss";
 
-const Home = (props) => <h1 className="mx-auto text-center">Home page</h1>;
+const Home = (props) => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col bg-light-blue bg-image-twitter">
+          <h1 className="mx-auto">Hello Friends</h1>
+        </div>
+        <div className="col">One of two columns</div>
+      </div>
+    </div>
+  );
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
